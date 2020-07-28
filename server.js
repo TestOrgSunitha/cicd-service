c442c3f1ab765056ec99abd6aabb24ca38a97041
Sunithagu <sunithagudisagar79@gmail.com>
2020-07-28 06:57:39 +0000

    Testing CICD

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Deployed Version is v2.0. I am testing CICD from google cloud build</h1>");
+  response.end("<h1> Deployed Version is v2.0. I am testing CICD from google cloud build, Please work</h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
