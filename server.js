efe356b241b54957543b6a822385672f55a2905b
Sunithagu <sunithagudisagar79@gmail.com>
2020-07-28 08:25:39 +0000

    Testing CI CD

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Deployed Version is v4.0 Testin Testing</h1>");
+  response.end("<h1> Testing CI CD</h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
